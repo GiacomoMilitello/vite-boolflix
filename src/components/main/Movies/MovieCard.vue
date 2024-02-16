f9adfbcc0d0ab6f7b01023a66ce893ea3026c797
GiacomoMilitello <g.militello95@gmail.com>
2024-02-16 17:30:36 +0100

    flags ok

--- src/components/main/Movies/MovieCard.vue
+++ src/components/main/Movies/MovieCard.vue
@@ -10,6 +10,20 @@ export default {
     },
     vote() {
       return Math.ceil(this.propsElement.vote_average / 2);
+    },
+    languageToCountryCode() {
+      return {
+        'en': 'us',
+        'ja': 'jp',
+        'he': 'il',
+        'zh': 'cn',
+        'ko': 'kr',
+        'hi': 'in',
+        'el': 'gr',
+      };
+    },
+    flagCode() {
+      return this.languageToCountryCode[this.propsElement.original_language] || this.propsElement.original_language;
     }
   },
   data(){
@@ -18,36 +32,24 @@ export default {
     }
   },
   methods: {
-    changeFlags(language){
-      switch (language) {
-        case 'en' : return 'us'
-        case 'ja' : return 'jp'
-        case 'he' : return 'il'
-        case 'zh' : return 'cn'
-        case 'ko' : return 'kr'
-        case 'hi' : return 'in'
-        default : return this.propsElement.original_language.toUpperCase()
-      }
-    }
+    
   }
 };
 </script>
 
 <template>
     
-  <div class="single-card m-2 d-flex align-items-center justify-content-center">
-    <figure>
+  <div class="single-card m-2 d-flex justify-content-center align-items-center">
+    <figure class="">
       <img :src="`https://image.tmdb.org/t/p/w185${propsElement.poster_path}`" alt="">
     </figure>
-    <ul class="">
+    <ul class="p-3">
         <li class=""> {{ isMovie ? propsElement.title : propsElement.name }} </li>
-        <li class="mt-3"> {{ isMovie ? propsElement.original_title : propsElement.original_name }} </li>
-        <li class="mt-3">
-          <figure>
-            <img :src="`https://flagsapi.com//${ changeFlags(propsElement.original_language).toUpperCase() }/flat/24.png`" width="24" alt="propsElement.original_language">
-          </figure>
+        <li class="mt-2"> {{ isMovie ? propsElement.original_title : propsElement.original_name }} </li>
+        <li class="mt-2">
+          <img :src="`https://flagcdn.com/w20/${flagCode}.png`" width="20" alt="flag">
         </li>
-        <li>
+        <li class="">
           <svg v-for="i in vote" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
           <svg v-for="i in 5 - vote" width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/></svg>
         </li>
@@ -62,10 +64,13 @@ export default {
 @use "../../../styles/partials/mixins" as *;
 
 .single-card{
-  background-color: black;
+  background-image: url(../../../img/back-card.avif);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
   width: 185px;
-  color: white;
   height: 278px;
+  color: white;
   cursor: pointer;
   svg{
     fill: gold;
@@ -75,6 +80,8 @@ export default {
   }
   ul{
     display: none;
+    background-color: rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
   }
   &:hover{
     figure{
